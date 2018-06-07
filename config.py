fc44ce3f30ad36f5bcaa6aa716f4f804b60d6a9d
michaelorgarza <36090715+michaelorgarza@users.noreply.github.com>
2018-06-06 20:47:03 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,10 @@
 breezometer_api_key_name = "Group4"
 breezometer_api_key_type = "Api Key"
 breezometer_api_key = "9b3014310bc040e7ae1a268d6bb4cd68"
+
+
+bm_key = "384395cd87304f83a8e00d110bba0177"
+an_key = "4DDE4D0E-3179-4B2D-A3F2-6FE964D107FD"
+ow_key = "6e6b0f450b212799c36cdc62fa58f591"
+
+pp_key = "UqTJ7L4SKQVGTaUQnVXFoq4TjPwz3ex4rXGkYGRc"
